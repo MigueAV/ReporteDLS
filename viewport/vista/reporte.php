dda913ed398061fcc49c1e3ee90753db6bf57ec3
MigueAV <103135724+MigueAV@users.noreply.github.com>
2022-04-07 14:51:55 -0500

    cambios reporte interface

--- viewport/vista/reporte.php
+++ viewport/vista/reporte.php
@@ -1,37 +1,54 @@
-<div class="card-body">
+<div class="card-body m-0 vh-100 row justify-content-center align-items-center">
 
-    <div class="row">
-        <div class="col-sm-2 mb-3">
-            <div class="row">
-                <div class="col-3">
-                    <label for="anio">AÑO:</label>
-                </div>
-                <div class="col">
-                    <select name="anio" id="anio" class="form-control form-control-sm">
-                        <?php
-                        for ($i = date('Y'); $i >= 2018; $i--) {
-                        ?>
-                            <option value="<?= $i; ?>"><?= $i; ?></option>
-                        <?php
-                        }
-                        ?>
-                    </select>
+    <div class="cuadro m-3 card-body ">
+        <div class="row">
+            <div class="col-sm mb-3">
+                <div class="title">
+                    <h5>REPORTE</h5>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6 mb-3">
-            <div class="row">
-                <div class="col-1">
-                    <label for="establecimiento">EESS:</label>
-                </div>
-                <div class="col">
-                    <select name="establecimiento" id="establecimiento" class="form-control form-control-sm"></select>
+
+        <div class="row">
+            <div class="col-sm mb-3">
+                <div class="row">
+                    <div class="col-1">
+                        <label for="anio">AÑO:</label>
+                    </div>
+                    <div class="col">
+                        <select name="anio" id="anio" class="form-control form-control-sm">
+                            <?php
+                            for ($i = date('Y'); $i >= 2018; $i--) {
+                            ?>
+                                <option value="<?= $i; ?>"><?= $i; ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm mb-3">
-            <button name="btnReporte" id="btnReporte" type="button" class="btn btn-sm btn-outline-dark">REPORTE PCPP</button>
+        <div class="row">
+            <div class="col-sm mb-3">
+                <div class="row">
+                    <div class="col-1">
+                        <label for="establecimiento">EESS:</label>
+                    </div>
+                    <div class="col">
+                        <select name="establecimiento" id="establecimiento" class="form-control form-control-sm"></select>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="row">
+            <div class="col-sm">
+                <button name="btnReporte" id="btnReporte" type="button" class="btn btn-sm btn-outline-dark">REPORTE PCPP</button>
+            </div>
+        </div>
+
     </div>
+
+
 
 </div>
