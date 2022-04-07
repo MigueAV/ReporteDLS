504aa27eb1ac4599e5951272473ee96d6871428c
MigueAV <103135724+MigueAV@users.noreply.github.com>
2022-04-07 10:25:12 -0500

    complementos viewport

---
+++ viewport/vista/js/function.js
@@ -0,0 +1,11 @@
+saltar = (campo, e) => {
+    if (e.keyCode == 13) {
+        $("#" + campo).focus();
+    }
+}
+
+seleccionar = (id) => {
+    valor_input = document.getElementById(id).value;
+    longitud = valor_input.length;
+    document.getElementById(id).setSelectionRange(0, longitud);
+}
