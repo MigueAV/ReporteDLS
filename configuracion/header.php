1a7a2360d97b907b0b52c119dfc3318fffbe0bb3
MigueAV <103135724+MigueAV@users.noreply.github.com>
2022-04-07 10:34:55 -0500

    cambios

---
+++ configuracion/header.php
@@ -0,0 +1,22 @@
+<head>
+    <title>Diris Lima Sur</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporte Diris Lima Sur</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="./bootstrap/fonts/css/font-awesome.min.css">
+</head>
+
+<script>
+    window.addEventListener("keypress", function(event){
+        if (event.keyCode === 13){
+            event.preventDefault();
+        }
+    }, false);
+    
+</script>
