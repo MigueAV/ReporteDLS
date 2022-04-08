857e01e0f19a08f0cf0cc66ba652113c959fe34f
MigueAV <103135724+MigueAV@users.noreply.github.com>
2022-04-08 08:31:47 -0500

    aa

--- viewport/vista/menu.php
+++ viewport/vista/menu.php
@@ -44,24 +44,16 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <a class="navbar-brand" href="#">Logo</a>
+    <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>    
-        </ul>
-    </div>  
+    </ul>
     </nav>
 
     
